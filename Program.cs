56970b1b98f74df8eb8b1a6479b23663d6cdc98b
Ilchan <annilchen@yandex.ru>
2022-10-15 16:17:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double c = 0;
+Console.WriteLine("Введите первый множитель:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второй множитель:");
+double b = Convert.ToDouble(Console.ReadLine());
+c = a;
+for(int i = 1; i < b; i++)
+{
+    c = c + a;
+}
+Console.WriteLine($"{a}*{b}={c}");
